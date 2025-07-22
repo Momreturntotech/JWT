465976f2b6d2d90d4c43aa5af283da55acc77b4b
Madhura BS <madhurabs@Madhuras-MacBook-Air.local>
2025-07-22 10:47:57 +0530

    Added Logout Controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -61,3 +61,19 @@ export const register= async(req,res)=>{
         }
 
     }
+
+
+export const logout =async(req,res)=>{    
+    try {
+        res.clearCookie('token',{
+            httpOnly:true,
+            secure:process.env.NODE_ENV==='production',
+            sameSite:process.env.NODE_ENV==='production'?'none':'strict',
+            
+        })
+        return  res.json({success:true,message:"Logged OUt"});
+    } catch (error) {
+        res.json({success:false,message:error.message})
+        }
+    
+}
