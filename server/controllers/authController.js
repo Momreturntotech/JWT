7d0f495cb7b338caca9b2659741c076eb9c3d3fb
Madhura BS <madhurabs@Madhuras-MacBook-Air.local>
2025-07-23 12:43:21 +0530

    added get user data and completed backend

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -165,3 +165,83 @@ export const verifyEmail=async(req,res)=>{
 
 
 }
+
+//Check if the user is Authenticated
+export const isAuthenticated= async(req,res)=>{
+    try {
+        return  res.json({success:true})
+    } catch (error) {
+         res.json({success:false,message:error.message});
+    }
+
+}
+
+
+export const sendResetOtp=async(req,res)=>{
+    const {email}=req.body;
+    if(!email){
+        return res.json({success:false,message:"Email is Required"})
+    }
+    try {
+        const user=await userModel.findOne({email});
+        if(!user){
+            return res.json({success:false,message:'User Not Found'});
+        }
+        const otp=String(Math.floor(1000000 + Math.random() * 900000));
+        user.resetOtp= otp;
+        user.resetOtpExpiresAt=Date.now() + 24 * 60 * 60 * 1000
+        await user.save();
+        const mailOptions={
+            from:process.env.SENDER_EMAIL,
+            to:user.email,
+            subject:"Password Reset Otp",
+            text:`Your Otp is ${otp} Verify your acoount using this reset otp}`
+
+        }
+        await transporter.sendMail(mailOptions);
+        return  res.json({success:true,message:"Otp sent to your email"});
+    } catch (error) {
+        res.json({success:false,message:error.message});
+        
+    }
+
+}
+
+
+export const resetPassword=async(req,res)=>{
+    const {email,otp,newPassword}=req.body
+    console.log("this is inside resetPassword",email,otp,newPassword)
+    if(!email || !otp || !newPassword){
+          return res.json({success:false,message:"email or opt or newpassword is missing"});
+
+    }
+    try {
+        const user = await userModel.findOne({ email});
+        if(!user){
+            return res.json({success:false,message:"User not found"});
+
+        }
+        if(user.resetOtp==="" || user.resetOtp !== otp){
+            return   res.json({success:false,message:"Invalid Otp"});
+        }
+
+        if(user.resetOtpExpiresAt<Date.now()){
+                 return res.json({success:false,message:"OTP Expired"});
+
+        }
+
+        const hashedPassword = await bcrypt.hash(newPassword,10);
+        user.password=hashedPassword
+        user.resetOtp='';
+        user.resetOtpExpiresAt=0;
+        await user.save();
+        return res.json({success:true,message:"Password has been reset successfully"});
+
+
+        
+    } catch (error) {
+         res.json({success:false,message:error.message});
+        
+    }
+
+}
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 
 import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 const app = express();
 const port = process.env.PORT || 4000
 connectDB();
@@ -15,4 +16,5 @@ app.use(cors({credentials:true}))
 //API Endpoints
 app.get('/',(req,res)=>res.send("API Working fine2"));
 app.use('/api/auth',authRouter)
+app.use('/api/user',userRouter)
 app.listen(port,()=>console.log("server started in theasdasd port ",port));
